d4d31a0cdb4b00e9a359ab0bfbe7c93c2ece8a02
Six <brbsix@gmail.com>
2016-06-05 15:34:42 -0700

    Update erroneous outdated version string

--- craigslist.py
+++ craigslist.py
@@ -248,7 +248,7 @@ def _parser(args):
         '--version',
         action='version',
         help=argparse.SUPPRESS,
-        version='%(prog)s 0.0.4')
+        version='%(prog)s 0.0.8')
 
     return parser.parse_args(args).cache
 
